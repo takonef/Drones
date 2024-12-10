0b8080e086db2d5c022e996bda9f77caa739dab4
takonef <takonef@gmail.com>
2024-12-10 15:03:09 +0300

    Update trace_point_pioneer.py

--- trace_point_pioneer.py
+++ trace_point_pioneer.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     camera = Camera()
     min_v = 1300
     max_v = 1700
+    frame_rate = 1
     try:
         while True:
             ch_1 = 1500
@@ -32,72 +33,70 @@ if __name__ == "__main__":
             bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
             a = np.array(bw_frame)
             a_size = a.shape
-
-
+        
             if frame is not None:
                 camera_frame = cv2.imdecode(
                     np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR
                 )
                 key = cv2.waitKey(1)
                 if key == ord("u"):
-
-                    time_previous = 0
-                    frame_rate = 1
-                    time_passed = time.time() - time_previous
-                    if time_passed > (
-                            1 / frame_rate):  # т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
-                        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                        thresh = 100
-                        bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
-                        time_previous = time.time()
-                        a = np.array(bw_frame)
-
-                        sum_rows = np.sum(a, axis=1)
-                        sum = 0
-                        am = 0
-                        for i in range(rows):
-                            if (255 * columns - sum_rows[i]) != 0:
-                                sum += i
-                                am += 1
-                        if am != 0:
-                            crow = sum // am
-                        else:
-                            crow = 0
-                            print("Bah")
-
-                        sum_columns = np.sum(a, axis=0)
-                        sum = 0
-                        am = 0
-                        for i in range(columns):
-                            if (255 * rows - sum_columns[i]) != 0:
-                                sum += i
-                                am += 1
-                        if am != 0:
-                            ccol = sum // am
-                        else:
-                            ccol = 0
-                            print("Bah")
-                        bw_frame[crow][ccol] = 255
-                        print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
-
-                        xc = columns / 2
-                        yc = rows / 2
-
-                        dx = xc - ccol
-                        dy = yc - crow
-                        dV_max = 400 #можно менять
-                        dVx = dV_max * (dx / xc)
-                        dVy = dV_max * (dy / yc)
-
-                        if dx < 70: #можно менять
-                            dx = 0
-                        else:
-                            ch_3 = 1500 + dVy
-                            ch_4 = 1500 + dVx
-
-                    cv2.imshow('live from your drone:)', bw_frame)
-                    if cv2.waitKey(1) == ord('q'):
-                        break
+                    while True:
+                        frame = camera.get_frame()
+                        time_previous = 0
+                        time_passed = time.time() - time_previous
+                        if time_passed > (1 / frame_rate):  # т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
+                            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                            thresh = 100
+                            bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
+                            time_previous = time.time()
+                            a = np.array(bw_frame)
+    
+                            sum_rows = np.sum(a, axis=1)
+                            sum = 0
+                            am = 0
+                            for i in range(rows):
+                                if (255 * columns - sum_rows[i]) != 0:
+                                    sum += i
+                                    am += 1
+                            if am != 0:
+                                crow = sum // am
+                            else:
+                                crow = 0
+                                print("Bah")
+    
+                            sum_columns = np.sum(a, axis=0)
+                            sum = 0
+                            am = 0
+                            for i in range(columns):
+                                if (255 * rows - sum_columns[i]) != 0:
+                                    sum += i
+                                    am += 1
+                            if am != 0:
+                                ccol = sum // am
+                            else:
+                                ccol = 0
+                                print("Bah")
+                            bw_frame[crow][ccol] = 255
+                            print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
+    
+                            xc = columns / 2
+                            yc = rows / 2
+    
+                            dx = xc - ccol
+                            dy = yc - crow
+                            dV_max = 400 #можно менять
+                            dVx = dV_max * (dx / xc)
+                            dVy = dV_max * (dy / yc)
+    
+                            if dx < 70: #можно менять
+                                dx = 0
+                            else:
+                                ch_3 = 1500 + dVy
+                                ch_4 = 1500 + dVx
+    
+                        cv2.imshow('live from your drone:)', bw_frame)
+                        if cv2.waitKey(1) == ord('q'):
+                            break
 
                 bw_frame.release()
                 cv2.destroyAllWindows()
