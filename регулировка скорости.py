aa14e69640b59b31b98140151789c5c1547126ba
vov4k <golovach.volodia@yandex.ru>
2025-01-16 17:36:46 +0300

    почти рабочий регулятор

--- регулировка скорости.py
+++ регулировка скорости.py
@@ -1,9 +1,8 @@
-rom
-pioneer_sdk
-import Pioneer, Camera
+from pioneer_sdk import Pioneer, Camera
 import cv2
 import numpy as np
 import time
+from median_trace import get_black_point
 
 if __name__ == "__main__":
     print(
@@ -28,81 +27,30 @@ if __name__ == "__main__":
             ch_3 = 1500
             ch_4 = 1500
             ch_5 = 2000
-            frame = camera.get_frame()
-            thresh = 100
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
-            a = np.array(bw_frame)
-            a_size = a.shape
+            frame = camera.get_cv_frame()
 
+            if frame is None:
+                continue
 
-            if frame is not None:
-                camera_frame = cv2.imdecode(
-                    np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR
-                )
-                key = cv2.waitKey(1)
-                if key == ord("u"):
+            xc, yc, _ = frame.shape
 
-                    time_previous = 0
-                    frame_rate = 1
-                    time_passed = time.time() - time_previous
-                    if time_passed > (
-                            1 / frame_rate):  # т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
-                        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                        thresh = 100
-                        bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
-                        time_previous = time.time()
-                        a = np.array(bw_frame)
+            ccol, crow = get_black_point(frame)
 
-                        sum_rows = np.sum(a, axis=1)
-                        sum = 0
-                        am = 0
-                        for i in range(rows):
-                            if (255 * columns - sum_rows[i]) != 0:
-                                sum += i
-                                am += 1
-                        if am != 0:
-                            crow = sum // am
-                        else:
-                            crow = 0
-                            print("Bah")
+            dx = xc - ccol
+            dy = yc - crow
+            print("dx, dy", dx, dy)
+            dV_max = 400
+            dVx = dV_max * (dx / xc)
+            dVy = dV_max * (dy / yc)
 
-                        sum_columns = np.sum(a, axis=0)
-                        sum = 0
-                        am = 0
-                        for i in range(columns):
-                            if (255 * rows - sum_columns[i]) != 0:
-                                sum += i
-                                am += 1
-                        if am != 0:
-                            ccol = sum // am
-                        else:
-                            ccol = 0
-                            print("Bah")
-                        bw_frame[crow][ccol] = 255
-                        print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
+            print("dVx, dVy", dVx, dVy)
 
-                        xc = columns / 2
-                        yc = rows / 2
+            if dx < 70:
+                dx = 0
+            else:
+                ch_3 = 1500 + int(dVy)
+                ch_4 = 1500 + int(dVx)
 
-                        dx = xc - ccol
-                        dy = yc - crow
-                        dV_max = 400
-                        dVx = dV_max * (dx / xc)
-                        dVy = dV_max * (dy / yc)
-
-                        if dx < 70:
-                            dx = 0
-                        else:
-                            ch_3 = 1500 + dVy
-                            ch_4 = 1500 + dVx
-
-                    cv2.imshow('live from your drone:)', bw_frame)
-                    if cv2.waitKey(1) == ord('q'):
-                        break
-
-                bw_frame.release()
-                cv2.destroyAllWindows()
 
             key = cv2.waitKey(1)
             if key == 27:  # esc
@@ -140,6 +88,9 @@ if __name__ == "__main__":
                 ch_1 = 2000
             elif key == ord("k"):
                 ch_1 = 1000
+
+
+            # print("ch3, ch4", ch_3, ch_4)
 
             pioneer_mini.send_rc_channels(
                 channel_1=ch_1,
