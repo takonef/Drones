145ccdca6a31bfc255f5a24e37deea7a2d284744
takonef <takonef@gmail.com>
2025-01-20 19:08:20 +0300

    Update and rename line_trace.py to line_trace_top-bottom.py

--- line_trace_top-bottom.py
+++ line_trace_top-bottom.py
@@ -19,11 +19,10 @@ def get_black_line(frame):
     blurred_frame = cv2.GaussianBlur(gray_frame, (19, 19), 0)
     bw_frame = cv2.adaptiveThreshold(blurred_frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 61, 15)
     a = np.array(bw_frame)
-    a_top = a[:len(a)//2] #верхняя часть массива
-    a_bottom = a[len(a)//2:] #нижняя часть массива
 
-    sum_rows_top = np.sum(a_top, axis=1)
-    sum_columns_top = np.sum(a_top, axis=0)
+    #верхняя часть массива
+    sum_rows_top = np.sum(a[:len(a)//2], axis=1)
+    sum_columns_top = np.sum(a[:len(a)//2], axis=0)
 
     sum1 = 0
     sum2 = 0
@@ -31,9 +30,6 @@ def get_black_line(frame):
     j = 0
     left = 0
     right = rows - 1
-    
-    # print("columns", columns)
-    # print("rows", rows)
     try:
         while i < (rows//2 - 1 - j):
             while sum1 <= sum2:
@@ -41,15 +37,15 @@ def get_black_line(frame):
                     sum1 += 1
                     left = i
                 i += 1
-                # print("i in while 1", i)
             while sum2 < sum1:
                 if (255 * columns - sum_rows_top[rows//2 - j - 1]) != 0:
                     sum2 += 1
                     right = rows//2 - 1 - j
                 j += 1
     except IndexError:
-        pass
         # print("выход за границе в первом")
+        pass
+
     ccol_top = (right + left) // 2
 
     sum1 = 0
@@ -58,7 +54,7 @@ def get_black_line(frame):
     j = 0
     left = 0
     right = columns - 1
-    # print(right, left)
+
     try:
         while i < (columns - 1 - j):
             while sum1 <= sum2:
@@ -66,36 +62,27 @@ def get_black_line(frame):
                     sum1 += 1
                     left = i
                 i += 1
-                # print("i in while 2", i)
             while sum2 <= sum1:
                 if (255 * rows//2 - sum_columns_top[columns - j - 1]) != 0:
                     sum2 += 1
                     right = columns - 1 - j
                 j += 1
     except IndexError:
-        pass
         # print("выход за границе в втором")
-    # print(right, left)
-    crow_top= (right + left) // 2
+        pass
 
-    frame = cv2.circle(bw_frame, (crow_top, ccol_top), 10, 155, thickness=20)
-    # print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
+    crow_top = (right + left) // 2
+    cv2.circle(bw_frame, (crow_top, ccol_top), 10, 155, thickness=20)
 
-    # print("imshow")
-
-
-    ###return (crow_top, ccol_top, bw_frame)
-
-    sum_rows_bottom = np.sum(a_bottom, axis=1)
-    sum_columns_bottom = np.sum(a_bottom, axis=0)
+    #нижняя часть массива
+    sum_rows_bottom = np.sum(a[len(a)//2:], axis=1)
+    sum_columns_bottom = np.sum(a[len(a)//2:], axis=0)
     sum1 = 0
     sum2 = 0
     i = 0
     j = 0
     left = 0
     right = rows//2 - 1
-    # print("columns", columns)
-    # print("rows", rows)
     try:
         while i < (rows//2 - 1 - j):
             while sum1 <= sum2:
@@ -103,15 +90,14 @@ def get_black_line(frame):
                     sum1 += 1
                     left = i
                 i += 1
-                # print("i in while 1", i)
             while sum2 < sum1:
                 if (255 * columns - sum_rows_bottom[rows//2 - j - 1]) != 0:
                     sum2 += 1
                     right = rows//2 - 1 - j
                 j += 1
     except IndexError:
-        pass
         # print("выход за границе в первом")
+        pass
     ccol_bottom = (right + left) // 2 + rows//2
 
     sum1 = 0
@@ -120,7 +106,6 @@ def get_black_line(frame):
     j = 0
     left = 0
     right = columns - 1
-    # print(right, left)
     try:
         while i < (columns - 1 - j):
             while sum1 <= sum2:
@@ -128,31 +113,22 @@ def get_black_line(frame):
                     sum1 += 1
                     left = i
                 i += 1
-                # print("i in while 2", i)
             while sum2 <= sum1:
                 if (255 * rows//2 - sum_columns_bottom[columns - j - 1]) != 0:
                     sum2 += 1
                     right = columns - 1 - j
                 j += 1
     except IndexError:
-        pass
         # print("выход за границе в втором")
-    # print(right, left)
+        pass
     crow_bottom = (right + left) // 2
+    cv2.circle(bw_frame, (crow_bottom, ccol_bottom), 10, 155, thickness=20)
 
-    bw_frame = cv2.circle(bw_frame, (crow_bottom, ccol_bottom), 10, 155, thickness=20)
     # print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
-
-    # print("imshow")
-
-
-    ###return (crow_bottom, ccol_bottom, bw_frame)
     color_for_arrow = (0, 0, 255)
-    bw_frame = cv2.arrowedLine(bw_frame, (ccol_bottom, crow_bottom), (ccol_top, crow_top), color_for_arrow, 2)
-    return (bw_frame)
-
-
-
+    bw_frame = cv2.arrowedLine(bw_frame, (crow_bottom, ccol_bottom), (crow_top, ccol_top), color_for_arrow, 2)
+    
+    return (bw_frame, ccol_bottom-ccol_top)
 
 image = cv2.VideoCapture(0)
 time_previous = 0 #не менять!!!!
@@ -173,23 +149,13 @@ while True:
     ret, frame = image.read()
     time_passed = time.time() - time_previous
     if time_passed > (1 / frame_rate): #т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
-        bw_frame = get_black_line(frame)
-        # bw_frame = cv2.circle(bw_frame, (ccol_bottom, crow_bottom), 10, 210, thickness=20)
-        # bw_frame = cv2.circle(bw_frame, (ccol_top, crow_top), 10, 210, thickness=20)
-        
-        
-        # print(crow, ccol)  # ряд(высота - у) и колонна(сторона - х)
-
+        bw_frame, delta = get_black_line(frame)
+        print(delta)
       
     cv2.imshow('live from your pc :)', bw_frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
-    # if key == ord('m'):
-    #     thresh -= 10
-    # if key == ord('p'):
-    #     thresh += 10
-
 
 image.release()
 cv2.destroyAllWindows()
