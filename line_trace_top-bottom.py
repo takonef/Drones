926fa91430626f0165aeceee30b5e344d774fc3a
takonef <takonef@gmail.com>
2025-01-21 17:05:28 +0300

    Update line_trace_top-bottom.py

--- line_trace_top-bottom.py
+++ line_trace_top-bottom.py
@@ -128,37 +128,37 @@ def get_black_line(frame):
     color_for_arrow = (0, 0, 255)
     bw_frame = cv2.arrowedLine(bw_frame, (crow_bottom, ccol_bottom), (crow_top, ccol_top), color_for_arrow, 2)
     
-    return (bw_frame, ccol_bottom-ccol_top)
+    return (bw_frame, -ccol_bottom+ccol_top)
 
-image = cv2.VideoCapture(0)
-time_previous = 0 #не менять!!!!
-frame_rate = 24 #можно менять
+# image = cv2.VideoCapture(0)
+# time_previous = 0 #не менять!!!!
+# frame_rate = 24 #можно менять
 
-thresh = 100
-ret, frame = image.read()
-gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
-bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
-a = bw_frame
-a_size = a.shape
+# thresh = 100
+# ret, frame = image.read()
+# gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
+# bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
+# a = bw_frame
+# a_size = a.shape
 
-rows = a_size[0]
-columns = a_size[1]
-print(rows, columns)
+# rows = a_size[0]
+# columns = a_size[1]
+# print(rows, columns)
 
-while True:
-    ret, frame = image.read()
-    time_passed = time.time() - time_previous
-    if time_passed > (1 / frame_rate): #т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
-        bw_frame, delta = get_black_line(frame)
-        print(delta)
+# while True:
+#     ret, frame = image.read()
+#     time_passed = time.time() - time_previous
+#     if time_passed > (1 / frame_rate): #т.е. мы хотим frame_rate кадров в секунду, тогда кадр появляется каждые 1/frame_rate секунды (получается перед показом каждого следующего кадра, мы проверяем, прошло ли 1/frame_rate секунд с показа предыдущего кадра)
+#         bw_frame, delta = get_black_line(frame)
+#         print(delta)
       
-    cv2.imshow('live from your pc :)', bw_frame)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
+#     cv2.imshow('live from your pc :)', bw_frame)
+#     key = cv2.waitKey(1)
+#     if key == ord('q'):
+#         break
 
-image.release()
-cv2.destroyAllWindows()
+# image.release()
+# cv2.destroyAllWindows()
 
 # if __name__ == "__main__":
 #     camera = Camera()
