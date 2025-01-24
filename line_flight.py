cadc67e3544afc420d1c9743cb76aa02891fd707
annkarodionova <rodionovanka@gmail.com>
2025-01-24 18:26:31 +0300

    Update line_flight.py

--- line_flight.py
+++ line_flight.py
@@ -11,10 +11,12 @@ from aruco_detect import *
 
 global_frame = None
 
+
 def camera_start():
     global global_frame
     while True:
         global_frame = get_cv_frame()
+
 
 th1 = threading.Thread(target=camera_start)
 th1.start()
@@ -49,7 +51,7 @@ if __name__ == "__main__":
     try:
         while True:
             frame = global_frame
-            print(global_frame)
+            #print(global_frame)
             if frame is not None:
                 ch_1 = 1500
                 ch_2 = 1500
@@ -57,40 +59,29 @@ if __name__ == "__main__":
                 ch_4 = 1500
                 ch_5 = 2000
 
-
-    
-
                 yc, xc, _ = frame.shape
                 yc //= 2
                 xc //= 2
                 yc -= 1
                 xc -= 1
-    
+
                 target_circle_x, frame_after_thresh, dx_center, dangle = get_black_line(frame, nearby_pixels, edge)
-    
-    
-    
-                    # print("dVx, dVy", dVx, dVy)
-                dVx_aruco, dVy_aruco, dx, dy = aruco_detected(detector, frame, xc, yc)
-                if dVx_aruco > 0 and dVy_aruco > 0:
-                    color_for_arrow = (255, 0, 0)
-                    frame = cv2.arrowedLine(frame, (xc, yc), (xc + dx, yc + dy), color_for_arrow, 2)
-                    ch_3 = 1500 + int(dVy_aruco)
-                    ch_4 = 1500 + int(dVx_aruco)
-                    ch_1 = 1400 #вниз
-    
-                dV_max = 300
+
+                # print("dVx, dVy", dVx, dVy)
+
+
+                dV_max = 200 #300
                 dVx = dV_max * (dx_center / xc)
                 # print(dangle)
-    
-                dVrot_k = 250
-    
-                limit = 170
+
+                dVrot_k = 150 #250
+
+                limit = 100
                 if dVx > limit:
                     dVx = limit
                 if dVx < -limit:
                     dVx = -limit
-    
+
                 if not is_control_by_PID:
                     dx = 0
                     color = [0, 0, 255]
@@ -100,21 +91,37 @@ if __name__ == "__main__":
                         print("you have reached the destination")
                         # time.sleep(2)
                         # pioneer_mini.land()
-    
+
                     ch_2 = 1500 - int(dVrot_k * dangle)
                     ch_4 = 1500 + int(dVx)
-                    ch_3 = 1430  # 1350
+                    ch_3 = 1425  # 1350
                     # ch_3 = 1500
                     color = [0, 255, 0]
                     frame = cv2.circle(frame, (10, 10), 10, color, cv2.FILLED)
-    
+
+                    dVx_aruco, dVy_aruco, dx, dy, ids = aruco_detected(detector, frame, xc, yc)
+                    if dVx_aruco is not None and dVy_aruco is not None:
+                        print("marker ids", ids)
+                        # if abs(dx) < xc // 4 and abs(dy) < yc // 4:
+                        is_control_by_PID = not is_control_by_PID
+
+                    #print(dVx_aruco, dVy_aruco)
+                dVx_aruco, dVy_aruco, dx, dy, ids = aruco_detected(detector, frame, xc, yc)
+                if dVx_aruco is not None and dVy_aruco is not None:
+                    color_for_arrow = (255, 0, 0)
+                    frame = cv2.arrowedLine(frame, (xc, yc), (xc + dx, yc + dy), color_for_arrow, 2)
+                    ch_3 = 1500 + int(dVy_aruco)
+                    ch_4 = 1500 + int(dVx_aruco)
+                    ch_1 = 1400  # вниз
+                    print(ch_1, ch_3, ch_4)
+
                 key = cv2.waitKey(1)
                 if key == 27:  # esc
                     print("esc pressed")
                     cv2.destroyAllWindows()
                     pioneer_mini.land()
                     break
-    
+
                 elif key == ord("5"):
                     nearby_pixels -= 10
                 elif key == ord("6"):
@@ -123,7 +130,7 @@ if __name__ == "__main__":
                     edge -= 2
                 elif key == ord("8"):
                     edge += 2
-    
+
                 elif key == ord("1"):
                     pioneer_mini.arm()
                 elif key == ord("2"):
@@ -156,9 +163,9 @@ if __name__ == "__main__":
                     ch_1 = 1000
                 elif key == ord("p"):
                     is_control_by_PID = not is_control_by_PID
-    
+
                 # print("ch3, ch2", ch_3, ch_2)
-    
+
                 pioneer_mini.send_rc_channels(
                     channel_1=ch_1,
                     channel_2=ch_2,
@@ -166,7 +173,7 @@ if __name__ == "__main__":
                     channel_4=ch_4,
                     channel_5=ch_5,
                 )
-    
+
                 cv2.imshow('live from your pc :)', frame_after_thresh)
                 cv2.imshow('without changes', frame)
     except Exception:
