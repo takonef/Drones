cd430426beefd81cf52563f2b67e7a6b0ac9056e
annkarodionova <rodionovanka@gmail.com>
2025-01-29 03:53:52 +0300

    Create черно белая трансляция с ноутбука.py

---
+++ куски/черно белая трансляция с ноутбука.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    thresh = 128
+    bw_frame = cv2.threshold(gray_frame, thresh, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('live from your pc :)', bw_frame)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
